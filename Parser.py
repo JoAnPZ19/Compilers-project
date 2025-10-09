35add8b1c287250fa7771031d0f8516b30643e8e
Queene Zavala Morales <queene.zavala.morales@intel.com>
2025-10-09 16:29:37 -0600

    adding majority of logic, it recognizes most python patterns

--- Parser.py
+++ Parser.py
@@ -26,270 +26,307 @@ class Parser:
         self.data = None
         self.debug = debug
         self.tokens = tokens
-        self.lexer = Lexer.Lexer(self.errors, debug=self.debug)
+        self.lexer = Lexer.IndentLexer(debug=self.debug)
         self.precedence = (
             ('left', 'OR'),
             ('left', 'AND'),
-            ('left', 'EQUAL', 'LESS', 'GREATER', 'GREATEREQUAL', 'LESSEQUAL', 'LESSGREATER'),
+            ('left', 'NOT'),
+            ('left', 'EQUAL', 'NOTEQUAL', 'LESS', 'GREATER', 'GREATEREQUAL', 'LESSEQUAL', 'IN', 'IS'),
             ('left', 'PLUS', 'MINUS'),
-            ('left', 'MULTI', 'DIVIDE', 'MOD'),
-            ('right', 'NOT', 'UMINUS'),
-)
-
-    def id_exists(self, symbol, p):
-        if not self.lexer.symbol_table.exists(symbol):
-            self.errors.append(f"Symbol '{symbol}' not declared", p.lexer.lineno, p.lexer.lexpos, 'semantic', self.data)
-            print(self.errors[-1])
+            ('left', 'MULTI', 'DIVIDE', 'MODULE', 'FDIVIDE'),
+            ('left', 'POW'),
+            ('right', 'UMINUS'),
+        )
 
     def p_error(self, p):
         if not p:
-            self.errors.append(f"Unexpected end of input", 0, 0, 'parser', self.data)
+            error_msg = f"Unexpected end of input"
+            self.errors.append(error_msg)
         else:
-            self.errors.append(f"Syntax error on '{p.value}'", p.lineno, p.lexpos, 'parser', self.data)
-        print(self.errors[-1])
+            error_msg = f"Syntax error on '{p.value}' at line {p.lineno}"
+            self.errors.append(error_msg)
+        print(f"Parser Error: {error_msg}")
 
     def parse_data(self, data):
         self.data = data
-        self.lexer.data = data
-        self.parser.parse(data, tracking=True)
+        self.lexer.input(data)
+        result = self.parser.parse(lexer=self.lexer)
+        return result
 
     def build(self):
-        self.tokens = tokens
-        self.precedence = getattr(self, 'precedence', ())
-        self.start = getattr(self, 'start', 'program')
         self.parser = yacc.yacc(module=self, debug=self.debug)
 
-    def p_program(self, p):
-        """program : PROGRAM ID body"""
-        p[0] = Node("program", p[2], [p[3]])
+    # Programa principal - módulo Python
+    def p_module(self, p):
+        """module : statements"""
+        p[0] = Node("module", None, p[1])
 
-    def p_body(self, p):
-        """body : DECLARE declaration_list BEGIN statement_list END"""
-        p[0] = Node("body", None, [Node("declarations", None, p[2]), Node("statements", None, p[4])])
-
-
-    def p_declaration_list(self, p):
-        """declaration_list : declaration SEMICOLON declaration_list
-                            | empty"""
-        if len(p) == 2:
-            p[0] = []
-        else:
-            p[0] = [p[1]] + p[3]
-
-    def p_declaration(self, p):
-        """declaration : type identifier_list"""
-        decl_nodes = [Node("identifier", name) for name in p[2]]
-        p[0] = Node("declaration", p[1], decl_nodes)
-
-    def p_identifier_list(self, p):
-        """identifier_list : ID COMMA identifier_list
-                           | ID"""
+    # Lista de statements
+    def p_statements(self, p):
+        """statements : statement
+                      | statements statement"""
         if len(p) == 2:
             p[0] = [p[1]]
         else:
-            p[0] = [p[1]] + p[3]
+            p[0] = p[1] + [p[2]]
 
-    def p_type(self, p):
-        """type : INTEGER
-                | DECIMAL"""
-        p[0] = p[1]
-
-    def p_statement_list(self, p):
-        """statement_list : statement SEMICOLON p_statement_list_single"""
-        p[0] = [p[1]] + (p[3] if p[3] else [])
-
-    def p_statement_list_single(self, p):
-        """p_statement_list_single : statement_list
-                                | empty"""
-        p[0] = p[1] if p[1] else []
-
+    # Statement individual
     def p_statement(self, p):
-        """statement :  if_statement
-                | assign_statement
-                | while_statement
-                | do_while_statement
-                | for_statement
-                | read_statement
-                | write_statement"""
-        p[0] = p[1]
-
-    def p_assign_statement(self, p):
-        """assign_statement : ID ASSIGN expression"""
-        self.id_exists(p[1], p)
-        p[0] = Node("assign", p[1], [p[3]])
-
-    def p_if_statement(self, p):
-        """if_statement : IF condition THEN statement_list if_statement_aux"""
-        if p[5] is None:
-            p[0] = Node("if", None, [p[2], Node("then", None, p[4])])
-        else:
-            p[0] = Node("if", None, [p[2], Node("then", None, p[4]), p[5]])
-
-    def p_if_statement_aux(self, p):
-        """if_statement_aux : END
-                            | ELSE statement_list END"""
-        if len(p) == 2:
-            p[0] = None
-        else:
-            p[0] = Node("else", None, p[2])
-
-    def p_condition(self, p):
-        """condition : expression"""
-        p[0] = p[1]
-
-    def p_do_while_statement(self, p):
-        """do_while_statement : DO statement_list WHILE condition"""
-        p[0] = Node("do-while", None, [Node("body", None, p[2]), p[4]])
-
-    def p_for_statement(self, p):
-        """for_statement : FOR assign_statement TO expression DO statement_list END"""
-        p[0] = Node("for", None, [p[2], p[4], Node("body", None, p[6])])
-
-    def p_while_statement(self, p):
-        """while_statement : WHILE condition DO statement_list END"""
-        p[0] = Node("while", None, [p[2], Node("body", None, p[4])])
-
-    def p_read_statement(self, p):
-        """read_statement : READ LPAREN ID RPAREN"""
-        self.id_exists(p[3], p)
-        p[0] = Node("read", p[3])
-
-    def p_write_statement(self, p):
-        """write_statement : WRITE LPAREN writable RPAREN"""
-        p[0] = Node("write", None, [p[3]])
-
-    def p_expression(self, p):
-        """expression : simple_expression expression_aux"""
-        if p[2] is None:
-            p[0] = p[1]
-        else:
-            p[0] = Node("relop", p[2].value, [p[1], p[2].children[0]])
-
-    def p_expression_aux(self, p):
-        """expression_aux : relop simple_expression
-                        | empty"""
+        """statement : simple_statement NEWLINE
+                     | compound_statement
+                     | NEWLINE"""
         if len(p) == 3:
-            p[0] = Node("relop", p[1], [p[2]])
-        else:
-            p[0] = None
-
-    def p_dual_expression(self, p):
-        """dual_expression : LPAREN expression RPAREN"""
-        p[0] = p[2]
-
-    def p_simple_expression(self, p):
-        """simple_expression : tern
-                            | dual_expression TERNAL simple_expression COLON simple_expression
-                            | simple_expression addop tern"""
-        if len(p) == 2:
             p[0] = p[1]
+        elif len(p) == 2 and p[1] is not None:
+            p[0] = p[1]
+        else:
+            p[0] = Node("pass")
+
+    # Statements simples (una línea)
+    def p_simple_statement(self, p):
+        """simple_statement : expression_statement
+                           | assignment_statement
+                           | return_statement
+                           | pass_statement"""
+        p[0] = p[1]
+
+    # Statements compuestos (con cuerpo indentado)
+    def p_compound_statement(self, p):
+        """compound_statement : function_def
+                             | if_statement
+                             | while_statement
+                             | for_statement"""
+        p[0] = p[1]
+
+    # Definición de función
+    def p_function_def(self, p):
+        """function_def : DEF ID LPAREN parameters RPAREN COLON suite"""
+        p[0] = Node("function_def", p[2], [Node("parameters", None, p[4]), p[7]])
+
+    def p_parameters(self, p):
+        """parameters : parameter_list
+                      | empty"""
+        if p[1] is None:
+            p[0] = []
+        else:
+            p[0] = p[1]
+
+    def p_parameter_list(self, p):
+        """parameter_list : ID
+                         | parameter_list COMMA ID"""
+        if len(p) == 2:
+            p[0] = [Node("parameter", p[1])]
+        else:
+            p[0] = p[1] + [Node("parameter", p[3])]
+
+    # Suite (cuerpo de función/estructura de control)
+    def p_suite(self, p):
+        """suite : NEWLINE INDENT statements DEDENT"""
+        p[0] = Node("suite", None, p[3])
+
+    # Statement if
+    def p_if_statement(self, p):
+        """if_statement : IF expression COLON suite
+                       | IF expression COLON suite elif_clauses
+                       | IF expression COLON suite elif_clauses ELSE COLON suite"""
+        if len(p) == 5:
+            p[0] = Node("if", None, [p[2], p[4]])
         elif len(p) == 6:
-            p[0] = Node("ternary", None, [p[1], p[3], p[5]])
+            p[0] = Node("if", None, [p[2], p[4], p[5]])
         else:
-            p[0] = Node("binop", p[2], [p[1], p[3]])
+            p[0] = Node("if", None, [p[2], p[4], p[5], p[8]])
 
-    def p_tern(self, p):
-        """tern : factor_a
-                | tern mulop factor_a"""
+    def p_elif_clauses(self, p):
+        """elif_clauses : elif_clause
+                       | elif_clauses elif_clause"""
+        if len(p) == 2:
+            p[0] = [p[1]]
+        else:
+            p[0] = p[1] + [p[2]]
+
+    def p_elif_clause(self, p):
+        """elif_clause : ELIF expression COLON suite"""
+        p[0] = Node("elif", None, [p[2], p[4]])
+
+    # Statement while
+    def p_while_statement(self, p):
+        """while_statement : WHILE expression COLON suite"""
+        p[0] = Node("while", None, [p[2], p[4]])
+
+    # Statement for
+    def p_for_statement(self, p):
+        """for_statement : FOR ID IN expression COLON suite"""
+        p[0] = Node("for", None, [Node("target", p[2]), p[4], p[6]])
+
+    # Return statement
+    def p_return_statement(self, p):
+        """return_statement : RETURN
+                           | RETURN expression"""
+        if len(p) == 2:
+            p[0] = Node("return")
+        else:
+            p[0] = Node("return", None, [p[2]])
+
+    # Pass statement
+    def p_pass_statement(self, p):
+        """pass_statement : PASS"""
+        p[0] = Node("pass")
+
+    # Assignment
+    def p_assignment_statement(self, p):
+        """assignment_statement : ID ASSIGN expression"""
+        p[0] = Node("assignment", p[1], [p[3]])
+
+    # Expression statement
+    def p_expression_statement(self, p):
+        """expression_statement : expression"""
+        p[0] = Node("expression_stmt", None, [p[1]])
+
+    # Expresiones
+    def p_expression(self, p):
+        """expression : binary_expression
+                     | unary_expression
+                     | comparison_expression
+                     | boolean_expression
+                     | primary"""
+        p[0] = p[1]
+
+    def p_binary_expression(self, p):
+        """binary_expression : expression PLUS expression
+                           | expression MINUS expression
+                           | expression MULTI expression
+                           | expression DIVIDE expression
+                           | expression FDIVIDE expression
+                           | expression MODULE expression
+                           | expression POW expression"""
+        p[0] = Node("binary_op", p[2], [p[1], p[3]])
+
+    def p_unary_expression(self, p):
+        """unary_expression : MINUS expression %prec UMINUS
+                           | NOT expression"""
+        p[0] = Node("unary_op", p[1], [p[2]])
+
+    def p_comparison_expression(self, p):
+        """comparison_expression : expression EQUAL expression
+                               | expression NOTEQUAL expression
+                               | expression LESS expression
+                               | expression GREATER expression
+                               | expression LESSEQUAL expression
+                               | expression GREATEREQUAL expression"""
+        p[0] = Node("comparison", p[2], [p[1], p[3]])
+
+    def p_boolean_expression(self, p):
+        """boolean_expression : expression AND expression
+                            | expression OR expression"""
+        p[0] = Node("boolean_op", p[2], [p[1], p[3]])
+
+    # Primarios
+    def p_primary(self, p):
+        """primary : atom
+                  | primary LPAREN arguments RPAREN
+                  | primary LBRACKET expression RBRACKET"""
         if len(p) == 2:
             p[0] = p[1]
+        elif p[2] == '(':
+            p[0] = Node("call", p[1].value if hasattr(p[1], 'value') else None, p[3])
         else:
-            p[0] = Node("binop", p[2], [p[1], p[3]])
+            p[0] = Node("subscript", None, [p[1], p[3]])
 
-    def p_factor_a(self, p):
-        """factor_a : factor 
-                    | NOT factor 
-                    | MINUS factor %prec UMINUS"""
+    def p_arguments(self, p):
+        """arguments : expression_list
+                    | empty"""
+        if p[1] is None:
+            p[0] = []
+        else:
+            p[0] = p[1]
+
+    def p_expression_list(self, p):
+        """expression_list : expression
+                          | expression_list COMMA expression"""
         if len(p) == 2:
-            p[0] = p[1]
+            p[0] = [p[1]]
         else:
-            p[0] = Node("unaryop", p[1], [p[2]])
+            p[0] = p[1] + [p[3]]
 
-    def p_factor(self, p):
-        """factor : ID 
-                | NUMBER 
-                | dual_expression"""
-        if p.slice[1].type == 'ID':
-            self.id_exists(p[1], p)
-            p[0] = Node("id", p[1])
-        elif p.slice[1].type == 'NUMBER':
-            p[0] = Node("number", p[1])
+    # Átomos
+    def p_atom(self, p):
+        """atom : ID
+               | NUMBER
+               | DECIMAL
+               | SSTRING
+               | DSTRING
+               | TRUE
+               | FALSE
+               | NONE
+               | LPAREN expression RPAREN"""
+        if len(p) == 2:
+            if p.slice[1].type == 'ID':
+                p[0] = Node("identifier", p[1])
+            elif p.slice[1].type in ('NUMBER', 'DECIMAL'):
+                p[0] = Node("number", p[1])
+            elif p.slice[1].type in ('SSTRING', 'DSTRING'):
+                p[0] = Node("string", p[1])
+            elif p.slice[1].type in ('TRUE', 'FALSE'):
+                p[0] = Node("boolean", p[1])
+            elif p.slice[1].type == 'NONE':
+                p[0] = Node("none")
         else:
-            p[0] = p[1]
-
-    def p_unary(self, p):
-        """unary : MINUS
-                 | NOT"""
-
-    def p_relop(self, p):
-        """relop : EQUAL
-                 | GREATER
-                 | GREATEREQUAL
-                 | LESS
-                 | LESSEQUAL
-                 | LESSGREATER"""
-
-    def p_addop(self, p):
-        """addop : PLUS
-                 | MINUS
-                 | OR"""
-
-    def p_mulop(self, p):
-        """mulop : MULTI
-                 | DIVIDE
-                 | MOD
-                 | AND"""
-
-    def p_constant(self, p):
-        """constant : NUMBER"""
+            p[0] = p[2]
 
     def p_empty(self, p):
         """empty :"""
+        p[0] = None
 
-    def parse(self, source, debug=False, tracking=False):
+    def parse(self, source, debug=False):
         self.errors = []
         self.lexer.input(source)
-        result = self.parser.parse(lexer=self.lexer, debug=debug, tracking=tracking)
+        result = self.parser.parse(lexer=self.lexer, debug=debug)
         return result
+
+# Función de prueba
+def test_parser():
+    test_code = """
+def random_operation(a, b):
+    return a * b + 2.6548
+
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    elif n == 0:
+        return n / 0
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+"""
+
+    parser = Parser(debug=False)
+    parser.build()
+    ast = parser.parse(test_code)
+    
+    print("=== AST GENERADO ===")
+    print(ast)
+    
+    if parser.errors:
+        print("\n=== ERRORES ===")
+        for error in parser.errors:
+            print(error)
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 2:
-        print("Uso: python Parser.py <archivo>")
-        sys.exit(1)
-
-    filename = sys.argv[1]
-    with open(filename, "r", encoding="utf-8") as f:
-        src = f.read()
-
-    p = Parser()
-    ast = p.parse(src)
-    print("\nAST:")
-    print(ast)
-    if p.errors:
-        print("\nSemantic Errors:")
-        for e in p.errors:
-            print(e)
-
-"""
-Fast testing call
-
-if __name__ == "__main__":
-    source = 
-    PROGRAM test
-    DECLARE
-        INTEGER x;
-        DECIMAL y;
-    BEGIN
-        x := 5;
-        IF x > 2 THEN
-            WRITE(x);
-        END
-    END
-  
-    p = Parser(debug=False)
-    p.build()
-    ast = p.parse(source)
-    print(ast)
-"""
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        with open(filename, "r", encoding="utf-8") as f:
+            src = f.read()
+        
+        parser = Parser(debug=False)
+        parser.build()
+        ast = parser.parse(src)
+        print("\n=== AST ===")
+        print(ast)
+        
+        if parser.errors:
+            print("\n=== ERRORES ===")
+            for error in parser.errors:
+                print(error)
+    else:
+        test_parser()
 
